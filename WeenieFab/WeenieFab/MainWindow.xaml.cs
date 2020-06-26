9517aa8aabf97ff554e9245f0d0726ce370ddf2f
harliq <30803300+harliq@users.noreply.github.com>
2020-06-26 13:38:36 -0400

    More logic

--- WeenieFab/WeenieFab/MainWindow.xaml.cs
+++ WeenieFab/WeenieFab/MainWindow.xaml.cs
@@ -24,56 +24,30 @@ namespace WeenieFab
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int SelectedItemID { get; set; }
+        // public int SelectedItemID { get; set; }
         public DataTable integerDataTable = new DataTable();
-        // public Dictionary<int, FoundItem> FoundItems { get; set; }
-        // public List<string> integerLists { get; set; } = new List<string>();
-
 
         public MainWindow()
         {
             InitializeComponent();
-            //CreateDictionary();
-            CreateIntegerLists("Don't need");
+            CreateWeenieTypeList();
+            CreateIntegerLists();
             CreateDataTable();
             dgInt32.DataContext = integerDataTable;
-            //cbWeenieType.ItemsSource = integerLists;
-        }
-
-        public class DataGridValues
-        {
-            public string Property { get; set; }
-            public string Value { get; set; }
-            public string Description { get; set; }
-        }
-
-        public void CreateDictionary()
-        {
-            IDictionary<int, string> dInt = new Dictionary<int, string>();
-
-            dInt.Add(1, "Item Type");
-            dInt.Add(2, "Creature Type");
-
-            // cbInt32.ItemsSource = dInt;
-
-
-            IDictionary<int, string> WeenieType = new Dictionary<int, string>();
-
-            WeenieType.Add(0, "Undefined");
-            WeenieType.Add(1, "Generic");
-            WeenieType.Add(2, "Clothing");
-            WeenieType.Add(3, "Missile Launcher");
-
-            cbWeenieType.ItemsSource = WeenieType;
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //public class DataGridValues
+        //{
+        //    public string Property { get; set; }
+        //    public string Value { get; set; }
+        //    public string Description { get; set; }
+        //}
+
+        private void btnAddInt32_Click(object sender, RoutedEventArgs e)
         {
             // search for duplicate property
 
-            // var integerDataTable = as new DataTable; DataGridVales;
-            //if (integerDataTable.Rows.Count < 1)
             if (SearchForDuplicateProps(integerDataTable, cbInt32Props.SelectedIndex.ToString()))
             {
                 MessageBox.Show("Property Already Exits");
@@ -81,9 +55,6 @@ namespace WeenieFab
             else
             {
                 string[] description = cbInt32Props.Text.Split(" ");
-
-                // var data = new DataGridValues { Property = cbInt32Props.SelectedIndex.ToString(), Value = tbValue.Text, Description = description[1] };
-
                 DataRow dr = integerDataTable.NewRow();
                 dr[0] = cbInt32Props.SelectedIndex.ToString();
                 dr[1] = tbValue.Text;
@@ -93,27 +64,27 @@ namespace WeenieFab
             }
 
         }
-        private static bool SearchForDuplicateProps(DataTable tempTable,string searchProp)
+
+        private static bool SearchForDuplicateProps(DataTable tempTable, string searchProp)
         {
             bool found = false;
 
             foreach (DataRow row in tempTable.Rows)
             {
                 if (row[0].ToString() == searchProp)
-                   found = true;
+                    found = true;
                 else
-                   found = false;
+                    found = false;
             }
 
             return found;
         }
-        
+
         internal class FoundItem
         {
             public int RecordID { get; set; }
             public string RecordName { get; set; }
         }
-
 
         public void CreateDataTable()
         {
@@ -122,40 +93,36 @@ namespace WeenieFab
             integerDataTable.Columns.Add(new DataColumn("Description"));
         }
 
-
-        //public void MyDictionary()
-        //{
-        //    FoundItems = new Dictionary<int, FoundItem>
-        //    {
-        //        { 1, new FoundItem() { RecordID = 1, RecordName = "Test Name 1" } },
-        //        { 2, new FoundItem() { RecordID = 2, RecordName = "Test Name 2" } },
-        //        { 3, new FoundItem() { RecordID = 3, RecordName = "Test Name 3" } },
-        //        { 4, new FoundItem() { RecordID = 4, RecordName = "Test Name 4" } }
-
-        //    };
-
-
-
-        //}
-
         private void btnInt32Remove_Click(object sender, RoutedEventArgs e)
         {
             var index = dgInt32.SelectedIndex;
             DataGridRow currentRowIndex = dgInt32.ItemContainerGenerator.ContainerFromIndex(index) as DataGridRow;
             DataRow dr = integerDataTable.Rows[currentRowIndex.GetIndex()];
             dr.Delete();
-            
+
             integerDataTable.AcceptChanges();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btnUpdateInt32_Click_1(object sender, RoutedEventArgs e)
         {
-            CreateIntegerLists("Don't need");
+            var index = dgInt32.SelectedIndex;
+            DataGridRow currentRowIndex = dgInt32.ItemContainerGenerator.ContainerFromIndex(index) as DataGridRow;
+            DataRow dr = integerDataTable.Rows[currentRowIndex.GetIndex()];
+
+
+            string[] description = cbInt32Props.Text.Split(" ");
+            
+            dr[0] = cbInt32Props.SelectedIndex.ToString();
+            dr[1] = tbValue.Text;
+            dr[2] = description[1];
+
+
+            integerDataTable.AcceptChanges();
         }
 
-        public void CreateIntegerLists(string filepath)
+        public void CreateIntegerLists()
         {
-            filepath = "Int32Properties.txt";
+            string filepath = "Int32Properties.txt";
 
             List<string> integerLists = new List<string>();
             foreach (string line in File.ReadLines(filepath))
@@ -163,13 +130,44 @@ namespace WeenieFab
                 integerLists.Add(line);
             }
             cbInt32Props.ItemsSource = integerLists;
+            cbInt32Props.SelectedIndex = 1;
+        }
+
+        public void CreateWeenieTypeList()
+        {
+            string filepath = "WeenieTypes.txt";
+
+            List<string> weenieTypeList = new List<string>();
+            foreach (string line in File.ReadLines(filepath))
+            {
+                weenieTypeList.Add(line);
+            }
+            cbWeenieType.ItemsSource = weenieTypeList;
+            cbWeenieType.SelectedIndex = 1;
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-        Regex regex = new Regex("[^0-9]+");
-        e.Handled = regex.IsMatch(e.Text);
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
+        private void dgInt32_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = dgInt32.SelectedIndex;
+            DataGridRow currentRowIndex = dgInt32.ItemContainerGenerator.ContainerFromIndex(index) as DataGridRow;
+            if (index + 1> integerDataTable.Rows.Count)
+            {
+
+            }
+            else
+            {
+                DataRow dr = integerDataTable.Rows[currentRowIndex.GetIndex()];
+                int cbindex = 0;
+                Int32.TryParse(dr[0].ToString(), out cbindex);
+                cbInt32Props.SelectedIndex = cbindex;
+                tbValue.Text = dr[1].ToString();
+            }
+        }
     }
 }
