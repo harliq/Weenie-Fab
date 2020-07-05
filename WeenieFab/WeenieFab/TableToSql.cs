b0fca321d23aec9f70a646cf649bb9f91a1742c0
harliq <30803300+harliq@users.noreply.github.com>
2020-07-05 11:27:10 -0400

    Added Attrib2 write, Skill write

--- WeenieFab/WeenieFab/TableToSql.cs
+++ WeenieFab/WeenieFab/TableToSql.cs
@@ -59,17 +59,17 @@ namespace WeenieFab
                 {
                     string sValue = "'" + row[1] + "'";
                     if (counter == 1 && counter == rowcount)
-                        sqltext += $" ({wcid},{row[0],3}, {sValue,1}) /* {row[2]} */;\n";
+                        sqltext += $" ({wcid},{row[0],4}, {sValue,1}) /* {row[2]} */;\n";
                     else if (counter == 1)
-                        sqltext += $" ({wcid},{row[0],3}, {sValue,1}) /* {row[2]} */\n";
+                        sqltext += $" ({wcid},{row[0],4}, {sValue,1}) /* {row[2]} */\n";
                     else
                     {
                         if (counter == rowcount)
-                            sqltext += $"     , ({wcid},{row[0],3}, {sValue,1}) /* {row[2]} */;\n";
+                            sqltext += $"     , ({wcid},{row[0],4}, {sValue,1}) /* {row[2]} */;\n";
                         //else if (counter == rowcount -1)
                         //    sqltext += "";
                         else
-                            sqltext += $"     , ({wcid},{row[0],3}, {sValue,1}) /* {row[2]} */\n";
+                            sqltext += $"     , ({wcid},{row[0],4}, {sValue,1}) /* {row[2]} */\n";
                     }
                     counter++;
                 }
@@ -105,6 +105,75 @@ namespace WeenieFab
                         //    sqltext += "";
                         else
                             sqltext += $"     , ({wcid},{row[0],4},{row[1],4},{row[2],2},{row[3],2}) /* {row[4]} */\n";
+
+                    }
+                    counter++;
+                }
+            }
+            if (rowcount > 0)
+                sqltext += $"\n";
+            else
+            {
+            }
+            return sqltext;
+        }
+
+        //  Attrib2 - Health, Stamina, Mana
+        public static string ConvertAttribute2Table(DataTable dt, string wcid, string header)
+        {
+            // dt.Clear();
+            string sqltext = "";
+
+            int counter = 1;
+            int rowcount = dt.Rows.Count;
+
+            if (rowcount > 0)
+            {
+                sqltext = header + $"\nVALUES";
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (counter == 1)
+                        sqltext += $" ({wcid},{row[0],4},{row[1],6},{row[2],2},{row[3],2},{row[4],5}) /* {row[5]} */\n";                    
+                    else
+                    {
+                        if (counter == rowcount)
+                            sqltext += $"     , ({wcid},{row[0],4},{row[1],6},{row[2],2},{row[3],2},{row[4],5}) /* {row[5]} */;\n";
+                        else
+                            sqltext += $"     , ({wcid},{row[0],4},{row[1],6},{row[2],2},{row[3],2},{row[4],5}) /* {row[5]} */\n";
+
+                    }
+                    counter++;
+                }
+            }
+            if (rowcount > 0)
+                sqltext += $"\n";
+            else
+            {
+            }
+            return sqltext;
+        }
+
+        public static string ConvertSkillsTable(DataTable dt, string wcid, string header)
+        {
+            // dt.Clear();
+            string sqltext = "";
+
+            int counter = 1;
+            int rowcount = dt.Rows.Count;
+
+            if (rowcount > 0)
+            {
+                sqltext = header + $"\nVALUES";
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (counter == 1)
+                        sqltext += $" ({wcid},{row[0],3},{row[1],2},{row[2],2},{row[3],2},{row[4],4},{row[5],2},{row[6],2}) /* {row[7]} */\n";
+                    else
+                    {
+                        if (counter == rowcount)
+                            sqltext += $"     , ({wcid},{row[0],3},{row[1],2},{row[2],2},{row[3],2},{row[4],4},{row[5],2},{row[6],2}) /* {row[7]} */;\n";
+                        else
+                            sqltext += $"     , ({wcid},{row[0],3},{row[1],2},{row[2],2},{row[3],2},{row[4],4},{row[5],2},{row[6],2}) /* {row[7]} */\n";
 
                     }
                     counter++;
