eae61c8c5a915ad589ea29b16f403c18bf39b075
harliq <30803300+harliq@users.noreply.github.com>
2020-11-22 18:56:23 -0500

    Adding Loot Profile Editor

---
+++ WeenieFab/WeenieFab/LootProfiler.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeenieFab
+{
+    /// <summary>
+    /// Interaction logic for LootProfiler.xaml
+    /// </summary>
+    public partial class LootProfiler : Window
+    {
+        public LootProfiler()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLootProfileClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
