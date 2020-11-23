b3d0d37521c586114e0403b2e27bea28c5d37c60
harliq <30803300+harliq@users.noreply.github.com>
2020-11-22 20:08:03 -0500

    Added Open/Save Dialogs

--- WeenieFab/WeenieFab/LootProfiler.xaml.cs
+++ WeenieFab/WeenieFab/LootProfiler.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using WeenieFab.Properties;
 
 namespace WeenieFab
 {
@@ -21,10 +14,35 @@ namespace WeenieFab
         {
             InitializeComponent();
         }
-
         private void btnLootProfileClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        private void btnLootProfileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open DeathTreasure File";
+            ofd.Filter = "All Weenie Types|*.sql|SQL files|*.sql";
+            ofd.InitialDirectory = WeenieFabUser.Default.DefaultSqlPath;
+            Nullable<bool> result = ofd.ShowDialog();
+
+            if (result == true)
+            {
+
+            }
+        }
+        private void btnLootProfileSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save DeathTreasure File";
+            sfd.Filter = "SQL files|*.sql";
+            sfd.InitialDirectory = WeenieFabUser.Default.DefaultSqlPath;
+            sfd.FileName = tbDeathTreasureDID.Text + $".sql";
+            Nullable<bool> result = sfd.ShowDialog();
+            if (result == true)
+            {
+
+            }
         }
     }
 }
