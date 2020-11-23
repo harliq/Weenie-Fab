3e2e42d06d4d0c0084da66edea4abfcaa5498682
harliq <30803300+harliq@users.noreply.github.com>
2020-11-23 17:04:31 -0500

    Added all the Tables

--- WeenieFab/WeenieFab/DeathTreasure.cs
+++ WeenieFab/WeenieFab/DeathTreasure.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace WeenieFab
 {
-    class DeathTreasure
+   public class DeathTreasure
     {
         public int ID { get; set; }
         public int DTdid { get; set; }
